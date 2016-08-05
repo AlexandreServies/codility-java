444d57e43eac8a2ff6733c66937e0473b98382f7
Alexandre Serviès <alex.servies@gmail.com>
2016-08-05 01:31:43 +0100

    Fixing CountDiv

--- src/lessons/prefixsums/CountDiv.java
+++ src/lessons/prefixsums/CountDiv.java
@@ -2,9 +2,6 @@ package lessons.prefixsums;
 
 public class CountDiv {
     public int solution(int A, int B, int K) {
-        return (int) (Math.floor((double) B / K) - Math.ceil((double) A / K))
-                + ((A % K == 0 || B % K == 0)
-                ? 1
-                : 0);
+        return (int) (Math.floor((double) B / K) - Math.ceil((double) A / K)) + 1;
     }
 }
