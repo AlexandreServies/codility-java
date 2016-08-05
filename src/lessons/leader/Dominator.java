28e1be5d905253a4a6e5e8eacf089953f6a124f5
Alexandre Serviès <alex.servies@gmail.com>
2016-08-05 07:15:23 +0100

    Dominator

---
+++ src/lessons/leader/Dominator.java
@@ -0,0 +1,30 @@
+package lessons.leader;
+
+import java.util.Stack;
+
+public class Dominator {
+    public int solution(int[] A) {
+        Stack<Integer> stack = new Stack<>();
+        for (int n : A) {
+            if (!stack.empty() && n != stack.peek())
+                stack.pop();
+            else
+                stack.push(n);
+        }
+        if (stack.empty())
+            return -1;
+
+        int candidate = stack.peek();
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < A.length; i++)
+            if (A[i] == candidate) {
+                count++;
+                index = i;
+            }
+        if (count > A.length / 2) {
+            return index;
+        }
+        return -1;
+    }
+}
