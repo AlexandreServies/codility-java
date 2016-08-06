547175ca56b3b69fc49acd48639b9ad5403d2361
Alexandre Serviès <alex.servies@gmail.com>
2016-08-06 22:42:13 +0100

    MaxSliceSum fixed negative numbers for good

--- src/lessons/maxslice/MaxSliceSum.java
+++ src/lessons/maxslice/MaxSliceSum.java
@@ -2,12 +2,11 @@ package lessons.maxslice;
 
 public class MaxSliceSum {
     public int solution(int[] A) {
-        int N = A.length;
-        int maxSlice = 0;
+        int maxSlice = A[0];
         int maxSliceEndingHere = 0;
 
         for (int n : A) {
-            maxSliceEndingHere = maxSliceEndingHere + n;
+            maxSliceEndingHere = n + Math.max(0, maxSliceEndingHere);
             maxSlice = Math.max(maxSlice, maxSliceEndingHere);
         }
         return maxSlice;
